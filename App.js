981a3ff60ee78a654e3ad01a8936ac5fca1afe10
Yi Chen Hock <yichenhock@gmail.com>
2020-06-25 16:06:04 +0100

    add icons and formatting

--- App.js
+++ App.js
@@ -14,40 +14,189 @@ import {
   View,
   Text,
   StatusBar,
+  TouchableOpacity,
+  Dimensions,
+  Image,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// import {
+//   Header,
+//   LearnMoreLinks,
+//   Colors,
+//   DebugInstructions,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
 
 import {RNCamera} from 'react-native-camera';
 
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVisible: true
+    }
+  }
+
   render(){
     return (
       <View style={styles.container}>
-        <RNCamera
-          style={{ flex: 1, alignItems: 'center' }}
-          ref={ref => {
-            this.camera = useRef
-          }}
-          // captureAudio={false}
-        />
+        <StatusBar backgroundColor="grey" />
+        
+        <View style={styles.container}>
+          {/* <TouchableOpacity 
+            style={{
+              flex:1,
+              alignItems: 'center',
+              backgroundColor: 'transparent'
+          }}>
+            <View
+            style={{
+              flex:1,
+              transform: [{scaleX: -1}] 
+          }}>
+              <RNCamera
+                style={{ 
+                  flex: 1, 
+                  alignItems: 'center'
+                }}
+                ref={ref => {
+                  this.camera = useRef
+                }}
+                captureAudio={false}
+              >
+              </RNCamera>
+            </View>
+
+          </TouchableOpacity> */}
+          
+          <RNCamera
+                style={{ 
+                  flex: 1, 
+                  alignItems: 'center'
+                }}
+                ref={ref => {
+                  this.camera = useRef
+                }}
+                captureAudio={false}
+              >
+              </RNCamera>
+          {
+            this.state.isVisible ?
+              <TouchableOpacity style={styles.tutorial} onPress={this.toggleVisible}>
+                <View style={{flexDirection: 'column', width:null, alignSelf:'center', backgroundColor:'transparent'}}>
+                  <View style={{flexDirection:'row', marginBottom:50}}>
+                    <Image
+                        style={{height: 120,resizeMode:'contain',marginRight:5}}
+                        source={require('./images/icons/double-tap.png')}
+                      />
+                    <Text style={{width: 200,color:"#fff",fontSize: 20, marginTop: 60}}>
+                      Double tap to flip horizontally
+                      </Text>
+                  </View>
+
+                  <View style={{flexDirection:'row'}}>
+                    <Image
+                        style={{height:120, resizeMode:'contain',marginRight:5}}
+                        source={require('./images/icons/tap.png')}
+                      />
+                    <Text style={{width: 200,color:"#fff",fontSize: 20, marginTop: 60}}>
+                      Tap and hold to flip horizontally until release
+                      </Text>
+                  </View>
+
+                </View>
+                {/* <Text style={styles.capture} onPress={this.takePicture.bind(this)}>press dis now or else</Text> */}
+              </TouchableOpacity>
+            :
+            null
+          }
+          
+          <View style={{position:'absolute', top:20, left:20}}>
+            <Image
+              style={{height:30, resizeMode:'contain'}}
+              source={require('./images/icons/dots.png')}
+            />
+
+          </View>
+          
+          <View style={{position:'absolute', top:20, right:0}}>
+            <Image
+              style={{height:30, resizeMode:'contain'}}
+              source={require('./images/icons/draw.png')}
+            />
+
+          </View>
+          
+          <View style={{flexDirection: 'row',position:'absolute', width:null, alignSelf:'center', bottom:25}}>
+            <Image
+              style={{height:30, width:30}}
+              source={require('./images/icons/switch-camera.png')}
+            />
+            <Image
+              style={{height:70, width:70}}
+              source={require('./images/icons/shutter-on.png')}
+            />
+            <Image
+              style={{height:30, width:30}}
+              source={require('./images/icons/split_screen.png')}
+            />
+          </View>
+          
+        </View>
+
+        
+      
       </View>
     )
   }
+
+  toggleVisible = () => {
+    this.setState(prevState => ({isVisible: !prevState.isVisible}));
+  }
+
+  takePicture() {
+    const options = {};
+    //options.location = ...
+    this.RNCamera.capture({metadata: options})
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+
+  flipCamera() {
+    <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[asdfasdfasdf]</Text>
+
+  }
+
 }
 
 const styles = StyleSheet.create({
+  tutorial: {
+    backgroundColor: 'rgba(105, 255, 246, 0.5)', 
+    position: 'absolute',
+    left: 0,
+    top: 0, 
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+    zIndex: 20,
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black'
+    flexDirection: 'column'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
   }
+
 })
 
 export default App
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # FlipApp
+
  An app for traditional artists to "flip" their canvas
